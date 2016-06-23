f17abadc334d83ad54de77fcfd9547ddd0b43eb8
Koba <kkare13@freeuni.edu.ge>
2016-06-23 22:06:30 +0400

    add sprinkler-switch classes

---
+++ src/main/java/ge/edu/freeuni/sdp/iot/service/sprinkler/service/SprinklerSwitch.java
@@ -0,0 +1,7 @@
+package ge.edu.freeuni.sdp.iot.service.sprinkler.service;
+
+/**
+ * Created by Koba on 23/06/2016.
+ */
+public interface SprinklerSwitch {
+}
---
+++ src/test/ge/edu/freeuni/sdp/iot/service/sprinkler/core/FakeSprinklerSwitch.java
@@ -0,0 +1,9 @@
+package ge.edu.freeuni.sdp.iot.service.sprinkler.core;
+
+import ge.edu.freeuni.sdp.iot.service.sprinkler.service.SprinklerSwitch;
+
+/**
+ * Created by Koba on 23/06/2016.
+ */
+public class FakeSprinklerSwitch implements SprinklerSwitch {
+}
